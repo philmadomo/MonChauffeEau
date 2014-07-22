e64761c3da5df46e754ab74772a561285e86d088
Philippe <philmadomo@free.fr>
2014-07-22 13:07:29 +0200

    Update MonChauffeEau.sql

--- MonChauffeEau.sql
+++ MonChauffeEau.sql
@@ -1,8 +1,3 @@
--- MySQL dump 10.13  Distrib 5.5.35, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: MonChauffeEau
--- ------------------------------------------------------
--- Server version	5.5.35-0ubuntu0.12.04.2-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -87,5 +82,3 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-07-15 15:38:53
